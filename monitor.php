f6df96537c84d20dbb90fa3ee0125b5e3e84365a
ninni13 <94174963+ninni13@users.noreply.github.com>
2023-10-17 02:51:11 +1000

    Update monitor.php

--- monitor.php
+++ monitor.php
@@ -1,8 +1,8 @@
 <?php
-// These functions are extracted from the script you provided earlier
-// ... [other functions like get_device_model, get_info, etc.]
+// Read settings from the JSON file
+$settings = json_decode(file_get_contents('settings.json'), true);
 
-function check_alerts() {
+function getSystemInfo() {
     global $D;
     $alerts = array();
 
@@ -31,8 +31,30 @@ function check_alerts() {
     return $alerts;
 }
 
-// Execute the check function
-$alerts = check_alerts();
+function check_system_status($settings) {
+    $alerts = array();
+
+    $data = getSystemInfo();
+
+    // CPU check
+    if ($settings['monitor_items']['cpu'] && $data['cpu']['percent'] > $settings['cpu_threshold']) {
+        $alerts[] = "High CPU usage: {$data['cpu']['percent']}%";
+    }
+
+    // RAM check
+    if ($settings['monitor_items']['ram'] && $data['mem']['percent'] > $settings['ram_threshold']) {
+        $alerts[] = "High RAM usage: {$data['mem']['percent']}%";
+    }
+
+    // Disk check
+    if ($settings['monitor_items']['disk'] && $data['disk']['percent'] > $settings['disk_threshold']) {
+        $alerts[] = "High Disk usage: {$data['disk']['percent']}%";
+    }
+
+    return $alerts;
+}
+
+$alerts = check_system_status($settings);
 ?>
 
 <!DOCTYPE html>
@@ -65,6 +87,30 @@ $alerts = check_alerts();
         }
     };
 </script>
+
+<!-- User Settings Form -->
+<form action="monitor.php" method="POST">
+    <label for="cpu_threshold">CPU Threshold (%):</label>
+    <input type="number" id="cpu_threshold" name="cpu_threshold" value="<?php echo $settings['cpu_threshold']; ?>" min="1" max="100">
+    
+    <label for="ram_threshold">RAM Threshold (%):</label>
+    <input type="number" id="ram_threshold" name="ram_threshold" value="<?php echo $settings['ram_threshold']; ?>" min="1" max="100">
+    
+    <label for="disk_threshold">Disk Threshold (%):</label>
+    <input type="number" id="disk_threshold" name="disk_threshold" value="<?php echo $settings['disk_threshold']; ?>" min="1" max="100">
+    
+    <input type="submit" value="Update Settings">
+</form>
+
+<?php
+// If form is submitted, update the settings.json file
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $settings['cpu_threshold'] = $_POST['cpu_threshold'];
+    $settings['ram_threshold'] = $_POST['ram_threshold'];
+    $settings['disk_threshold'] = $_POST['disk_threshold'];
+    file_put_contents('settings.json', json_encode($settings));
+}
+?>
 
 </body>
 </html>
